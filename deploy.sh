d9fb4e3671e154cff2d4cc847c9a8a6bdf1e1449
Dat Dinhquoc <datdinhquoc@hotmail.com>
2020-01-25 10:57:59 +0000

    Msg

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set +ex;
 
-#Check MongoDB installation
-Count=$(mongo <<<exit | grep "command not found" | wc -l);
-echo "MongoDB installed: $Count";
-
-if [[ $Count == 0 ]]; then
-  sudo yum install mongodb-org;
-fi
-
 #Restart app
 git pull && git add -A && git commit -a -m Msg && git push;
 heroku logs -ta gdfile;
--- server.sh
+++ server.sh
@@ -2,7 +2,15 @@
 set +ex;
 
 echo "================================================================================";
-mongo <<<exit;
+#Check MongoDB installation
+Count=$(mongo <<<exit | grep "command not found" | wc -l);
+echo "MongoDB installed: $Count";
+
+if [[ $Count == 0 ]]; then
+  sudo yum install mongodb-org;
+fi
+
+#Start app
 node -v;
 node --experimental-modules server.mjs;
 #EOF
